7af731b7ee0700190cc022c38aa9d1283d0f07db
Álex Sáez <alexsaezm@gmail.com>
2023-05-08 11:17:36 +0200

    Add flatpak shorcuts and update i3

--- i3/.screenlayout/docked.sh
+++ i3/.screenlayout/docked.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --pos 3840x687 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-1-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --off
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 3840x1080 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-1-1 --off --output DP-1-1-5 --off --output DP-1-1-6 --off --output DP-1-2 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1-3 --off
