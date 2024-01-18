8e7c459e431a20301aababc77e3aad61fdb8bcac
Álex Sáez <alexsaezm@gmail.com>
2024-01-18 17:29:25 +0100

    Clean up nvim folder

--- nvim/.config/nvim/lua/plugins/theme.lua
+++
@@ -1,9 +0,0 @@
-return {
-    -- I love tempus themes so much that
-    -- I wanted to have a backup of them
-    'alexsaezm/tempus-themes-vim',
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme 'tempus_classic'
-    end,
-}
